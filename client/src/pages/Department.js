9d804c2bbbeeaf2b312cd290864ed630c78fb7e6
alexandrezagame <zagamealexandre@gmail.com>
2020-12-01 10:15:29 +0100

    creating the development page

---
+++ client/src/pages/Department.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Department = () => {
+    return (
+        <div>
+            
+            <h1>MY DEPARTMENT</h1>
+        </div>
+    )
+}
+
+export default Department
